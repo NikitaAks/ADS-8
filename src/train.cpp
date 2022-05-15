31de2d2ba831566bf7224d1b8b303e55e8daa66d
NikitaAks <99181243+NikitaAks@users.noreply.github.com>
2022-05-16 00:46:13 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,54 +1,39 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-
-Train::Train(): countOp(0), first(nullptr) {}
-
-void Train::addCage(bool glim) {
-      Cage *cage = new Cage;
-      cage->glim = glim;
-      cage->next = nullptr;
-      cage->prev = nullptr;
-        if (first == nullptr) {
-            first = cage;
-        } else if (first->prev == nullptr) {
-          first->prev = cage;
-          cage->next = first;
-          first->next = cage;
-          cage->prev = first;
-        } else {
-          first->next->prev = cage;
-          cage->next = first->next;
-          first->next = cage;
-          cage->prev = first;
-      }
-  }
-
+#include <iostream>
+void Train::addCage(bool light) {
+    Cage* temp = create(light);
+    if (isEmpty()) {
+       current = first = tail = temp;
+    } else {
+        tail->next = temp;
+        tail->next->prev = tail;
+        tail = tail->next;
+        first->prev = temp;
+        tail->next = first;
+    }
+}
 int Train::getLength() {
-      int len = 0;
-      int count = 0;
-      first->glim = true;
-      Cage *temp = first;
-        while (true) {
+    if (current == first && !first->light)
+            first->light = true;
+    while (true) {
+        int lennn = 0, templ = 0;
+        do {
+            lennn++;
             countOp++;
-            len++;
-            temp = temp->prev;
-            if (temp->glim) {
-                temp->glim = false;
-                count = 0;
-                while (count < len) {
-                    temp = temp->next;
-                    countOp++;
-                    count++;
-              }
-                if (!temp->glim) {
-                    return len;
-              }
-              len = 0;
-          }
-      }
-      return count;
-  }
-
+            current = current->next;
+        } while (!current->light);
+        current->light = false;
+        templ = lennnn;
+        while (lennnn--) {
+            countOp++;
+            current = current->prev;
+        }
+        if (!current->light) {
+            return templ;
+        }
+    }
+}
 int Train::getOpCount() {
-      return countOp;
-  }
+    return countOp;
+}

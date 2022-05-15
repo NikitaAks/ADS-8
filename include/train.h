77b71d4bb6eddc0d0966738f5353c83376d586cd
NikitaAks <99181243+NikitaAks@users.noreply.github.com>
2022-05-16 00:26:42 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,6 +1,9 @@
+@@ -1,20 +1,55 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
+// Copyright 2021 NNTU-CS
+#include "train.h"
 
 class Train {
  private:
@@ -18,3 +21,55 @@ class Train {
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
 };
 #endif  // INCLUDE_TRAIN_H_
+
+Train::Train(): countOp(0), first(nullptr) {}
+
+void Train::addCage(bool light) {
+  Cage *cage = new Cage;
+  cage->light = light;
+  cage->prev = nullptr;
+  cage->next = nullptr;
+  if (first == nullptr) {
+    first = cage;
+  } else if (first->next == nullptr) {
+    first->next = cage;
+    cage->prev = first;
+    first->prev = cage;
+    cage->next = first;
+  } else {
+    first->prev->next = cage;
+    cage->prev = first->prev;
+    first->prev = cage;
+    cage->next = first;
+  }
+}
+
+int Train::getLength() {
+  int dlin = 0;
+  int tr_dlin;
+  countOp = 0;
+  first->light = true;
+  Cage* temp = first;
+  while (true) {
+    countOp++;
+    dlin++;
+    temp = temp->next;
+    if (temp->light) {
+      temp->light = false;
+      tr_dlin = dlin;
+      for (tr_dlin; tr_dlin > 0; tr_dlin--) {
+        temp = temp->prev;
+        countOp++;
+      }
+      if (!temp->light) {
+        return dlin;
+      }
+      dlin = tr_dlin;
+    }
+  }
+  return tr_dlin;
+}
+
+int Train::getOpCount() {
+  return countOp;
+}
